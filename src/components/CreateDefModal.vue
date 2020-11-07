098b647e685fd38cdfb791c87ff850edc9f0469a
lucharrier <lucharrier@gmail.com>
2020-11-07 21:08:36 +0100

    Used bulma in form

--- src/components/CreateDefModal.vue
+++ src/components/CreateDefModal.vue
@@ -6,33 +6,33 @@
         @cancel="handleCancel"
     >
         <form @submit.prevent="submit">
-            <div>
-                <label for="name">
+            <div class="field">
+                <label class="label" for="name">
                     word
                 </label>
-                <input type="text" name="name" id="name" v-model="form.name">
+                <input class="input" type="text" name="name" id="name" v-model="form.name">
             </div>
-            <div>
-                <label for="text">
+            <div class="field">
+                <label class="label" for="text">
                     definition
                 </label>
-                <textarea type="text" name="text" id="text" v-model="form.text"/>
+                <textarea class="textarea" type="text" name="text" id="text" v-model="form.text"/>
             </div>
-            <div>
-                <label for="text">
+            <div class="field">
+                <label class="label" for="text">
                     exemple
                 </label>
-                <textarea type="text" name="exemple" id="exemple" v-model="form.exemple"/>
+                <textarea class="textarea" type="text" name="exemple" id="exemple" v-model="form.exemple"/>
             </div>
-            <a-select mode="tags" style="width: 100%" :token-separators="[',']" @search="handleSearch" @change="handleChange">
+            <!-- <a-select mode="tags" style="width: 100%" :token-separators="[',']" @search="handleSearch" @change="handleChange">
                 <a-select-option v-for="tag in tags" :key="tag.id">
                     {{ tag.name }}
                 </a-select-option>
-            </a-select>
-            <div>
-                <button type="submit">
-                    signin
-                </button>
+            </a-select> -->
+            <div class="field is-grouped">
+                <div class="control">
+                    <button class="button is-link" type="submit">Submit</button>
+                </div>
             </div>
         </form>
     </a-modal>
@@ -41,7 +41,7 @@
     import { mapActions } from 'vuex'
     import { 
         Modal,
-        Select,
+        // Select,
     } from 'ant-design-vue'
 
     import Vue from 'vue';
@@ -51,8 +51,8 @@
         name: 'dasboard',
         components: {
             // 'a-modal': Modal,
-            'a-select': Select,
-            'a-select-option': Select.Option
+            // 'a-select': Select,
+            // 'a-select-option': Select.Option
         },
         created() {
             console.log('LCS CRATED ')
