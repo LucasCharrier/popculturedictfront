bc2ad36afb81dbad0203d5776a40c4481d2e7d4e
lucharrier <lucharrier@gmail.com>
2020-11-08 14:55:15 +0100

    Used env variable for api

--- src/config.js
+++ src/config.js
@@ -1,3 +1,2 @@
-export const API_BASE_URL = 'http://localhost:8000/api';
-export const TRIVIA_ENDPOINT = 'http://jservice.io/api/random?count=1';
+export const API_BASE_URL = `${process.env.VUE_APP_API_URL}`;
 
