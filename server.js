988e88b9834be43776289f8a9bcaf361382d577c
lucharrier <lucharrier@gmail.com>
2020-11-11 22:30:26 +0100

    Added server

---
+++ server.js
@@ -0,0 +1,57 @@
+// server.js    
+const express = require('express');
+const path = require('path');
+const fs = require('fs');
+const vueServerRenderer = require('vue-server-renderer');
+const setupDevServer = require('./config/setup-dev-server');
+ 
+const port = 3000;
+const app = express();
+ 
+const createRenderer = (bundle) =>
+   vueServerRenderer.createBundleRenderer(bundle, {
+       runInNewContext: false,
+       template: fs.readFileSync(
+           path.resolve(__dirname, 'index.html'),
+           'utf-8'
+       )
+   });
+let renderer;
+ 
+// you may want to serve static files with nginx or CDN
+app.use('/public',  express.static(path.resolve(__dirname, './dist')));
+ 
+if (process.env.NODE_ENV === 'development') {
+    setupDevServer(app, (serverBundle) => {
+        renderer = createRenderer(serverBundle);
+    });
+} else {
+    renderer = createRenderer(
+        require('./dist/vue-ssr-server-bundle.json')
+    );
+}
+ 
+app.get('*', async (req, res) => {
+   const context = {
+       url: req.url,
+       state: {
+           title: 'Vue SSR Simple Steps',
+           users: []
+       }
+   };
+   let html;
+   console.log('LCS SERVER',req.url)
+   try {
+       html = await renderer.renderToString(context);
+   } catch (error) {
+        console.log('LCS EEROR 404 OU', error);
+       if (error.code === 404) {
+           return res.status(404).send('404 | Page Not Found');
+       }
+       return res.status(500).send('500 | Internal Server Error');
+   }
+ 
+   res.end(html);
+});
+ 
+app.listen(port, () => console.log(`Listening on: ${port}`));
