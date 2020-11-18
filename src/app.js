521df350e090528b7a2a1a6a6b4d298bc47103a1
lucharrier <lucharrier@gmail.com>
2020-11-18 10:51:47 +0100

    Added ssr app id

--- src/app.js
+++ src/app.js
@@ -15,7 +15,9 @@ axios.defaults.baseURL = 'https://api-therealdictionnary.herokuapp.com/api'
 Vue.config.productionTip = false
 Vue.use(PortalVue)
 Vue.use(VueHtml2Canvas)
-Vue.use(VueMeta);
+Vue.use(VueMeta, {
+  ssrAppId: 1
+});
 
 
 export const createApp = (context, modules) =>  {
